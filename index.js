0ec23bc78d7511b5317c819523b1ba6b3a066493
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2023-03-29 12:02:18 +0200

    Structured the new phones section and added owl carousel.

--- index.js
+++ index.js
@@ -31,9 +31,27 @@ $(document).ready(function () {
     });
 
     //filter items on button click
-    $(".btn-group").onclick("click", "button", function (){
+    $(".btn-group").on("click", "button", function (){
         var filterValue = $(this).attributes('data-filter');
         $grid.isotope({filter: filterValue});
     })
+
+    //new phones owl carousel
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
 
 });
